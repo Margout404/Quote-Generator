271465e0b2badb2abb4e52b940a52eca0f537e8e
Margout404 <mariosgoutB5@gmail.com>
2025-01-09 00:47:06 +0200

    final

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+const quotecontainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitbut = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
 let apiQuotes = [];
+//loading
+function loading(){
+    loader.hidden=false
+    quotecontainer.hidden=true
+}
+function complete(){
+    loader.hidden=true  
+    quotecontainer.hidden=false
+}
+
+
 //fynction for calling new quotes
 function newQuote(){
+    //start the loading spinner
+    loading()
     // pick a random quote from apiquotes array
     const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
-    console.log(quote);
+    //check if author is available
+    if (quote.author==null){
+        authorText.textContent = 'Unkown'
+    } else {
+        authorText.textContent = quote.author
+    }
+    //if its gonna change the style or not
+    if (quote.text.length>140){
+        quoteText.classList.add('long-quote')
+    } else{
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
+    complete()
+    //complete the loading symbol
+
+
+
+
 }
 
 //api quotes
@@ -18,5 +56,14 @@ async function getQuotes() {
     }
     
 }
+
+function tweetQuote(){
+    const twitterUrl= `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl,'_blank')
+}
+//event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+
+twitbut.addEventListener('click',tweetQuote)
 //on load
 getQuotes();
