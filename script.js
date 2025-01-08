cc924b202259592ded2d10098dfd5f8e09d4aa67
Margout404 <mariosgoutB5@gmail.com>
2025-01-08 18:10:54 +0200

    almost ready

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testtt')
+let apiQuotes = [];
+//fynction for calling new quotes
+function newQuote(){
+    // pick a random quote from apiquotes array
+    const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
+    console.log(quote);
+}
+
+//api quotes
+async function getQuotes() {
+    const apiUrl='https://jacintodesign.github.io/quotes-api/data/quotes.json'
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch (error){
+        //catch the error
+    }
+    
+}
+//on load
+getQuotes();
